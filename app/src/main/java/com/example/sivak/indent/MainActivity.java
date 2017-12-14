f3b2e602faf2b37168567f59f7035d591b5fe00f
sivakarthick <sivakarthick308@gmail.com>
2017-12-14 23:21:04 +0530

    First commit

---
+++ app/src/main/java/com/example/sivak/indent/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sivak.indent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
